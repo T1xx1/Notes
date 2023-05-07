c4223848fc65db8e612d840e9d63454421658724
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2023-05-07 20:22:02 +0200

    Tailwind setup

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 import App from './app/_index.tsx';
 
+import './index.scss';
+
 ReactDOM.createRoot(document.querySelector('#root') as HTMLElement).render(
    <React.StrictMode>
       <App />

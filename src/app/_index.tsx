dee1bbe74c1e9c94633ee6faf74f6fe209f89c28
T1xx1 <69322987+T1xx1@users.noreply.github.com>
2023-05-09 23:19:36 +0200

    Main component

--- src/app/_index.tsx
+++ src/app/_index.tsx
@@ -1,11 +1,13 @@
 import useStore from '../hooks/usestore';
 
 import Footer from './footer/_index';
+import Main from './main/_index';
 
 export default function App() {
    let [val, dispatch] = useStore();
-   
+
    return <>
+      {val !== null && <Main val={Object.values(val)} dispatch={dispatch} />}
       <Footer />
    </>;
 }
---
+++ src/app/main/_index.tsx
@@ -0,0 +1,14 @@
+import { Note as NoteClass } from '../../hooks/usestore';
+
+import Note from './note';
+
+export default function Main({
+   val,
+   dispatch
+}: {
+   val: any,
+   dispatch: Function;
+}) {
+   return <main>
+   </main>;
+}
